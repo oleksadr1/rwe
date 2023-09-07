96020ac557b53b65bd0f48e570561c7c64abe2ed
Саша Петрас <petrasoleksandr2006@GMAIL.com>
2023-09-07 23:23:33 +0200

    dzień 4

--- Challgeapp/Program.cs
+++ Challgeapp/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string name = "Oleksandr";
+string Famale = "mężczyzna";
+int Age = 25;
+
+if (Famale == "mężczyzna" && Age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Oleksandr" && Age > 30) 
+{
+    Console.WriteLine("Ewa lat 30 ");
+}
+else if (Famale == "mężczyzna" && Age < 18)
+{
+    Console.WriteLine("Niepewnoletni mężczyzna ");
+}
