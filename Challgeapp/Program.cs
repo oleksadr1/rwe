65550e5284e49153b9dda81f514e4a9b9a0d7371
Саша Петрас <petrasoleksandr2006@GMAIL.com>
2023-09-09 20:00:32 +0200

    dzień 5

--- Challgeapp/Program.cs
+++ Challgeapp/Program.cs
@@ -1,18 +1,74 @@
 ﻿using System;
+int number = 4566;
+string numberAsString =number. ToString();
+char[] charletters = numberAsString.ToCharArray();
 
-string name = "Oleksandr";
-string Famale = "mężczyzna";
-int Age = 25;
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0; 
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+;
+foreach (char c in charletters)
+{
+    if (c == '0')
+    {
+        counter0++;
+    }
+    if (c =='1')
+    {
+        counter1++;
+    }
+    if (c == '2')
+    {
+        counter2++;
+    }
 
-if (Famale == "mężczyzna" && Age < 30)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat");
-}
-else if (name == "Oleksandr" && Age > 30) 
-{
-    Console.WriteLine("Ewa lat 30 ");
-}
-else if (Famale == "mężczyzna" && Age < 18)
-{
-    Console.WriteLine("Niepewnoletni mężczyzna ");
+    if (c == '3')
+    {
+        counter3++;
+    }
+
+    if (c == '4')
+    {
+        counter4++;
+    }
+    if (c == '5')
+    {
+        counter5++;
+    }
+
+    if (c == '6')
+    {
+        counter6++;
+    }
+    if (c == '7')
+    {
+        counter7++;
+    }
+    if (c == '8')
+    {
+        counter8++;
+    }
+    if (c == '9')
+    {
+        counter9++;
+    }
+    Console.WriteLine("0=>" + counter0);
+    Console.WriteLine("1=>" + counter1);
+    Console.WriteLine("2=>" + counter2);
+    Console.WriteLine("3=>" + counter3);
+    Console.WriteLine("4=>" + counter4);
+    Console.WriteLine("5=>" + counter5);
+    Console.WriteLine("6=>" + counter6);
+    Console.WriteLine("7=>" + counter7);
+    Console.WriteLine("8=>" + counter8);
+    Console.WriteLine("9=>" + counter9);
+   
+
 }
